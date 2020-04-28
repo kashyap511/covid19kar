7900c38d2c7c98afe0449059c9e25f656e07c790
Srilakshmi Shastri <pramod.ram.kashyap@gmail.com>
2020-04-28 17:27:14 +0530

    Updates

---
+++ precache-manifest.ccfc7f8fce297953a9d0f20d196688c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8c44d833d3acda479e0e39c3bdc418",
+    "url": "/covid19india/index.html"
+  },
+  {
+    "revision": "2ac06564e5564fda7676",
+    "url": "/covid19india/static/css/main.3ad124ce.chunk.css"
+  },
+  {
+    "revision": "6662b6045b560e96f0d2",
+    "url": "/covid19india/static/js/2.c3a6a9af.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/covid19india/static/js/2.c3a6a9af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac06564e5564fda7676",
+    "url": "/covid19india/static/js/main.16670738.chunk.js"
+  },
+  {
+    "revision": "4199556c759b36181783",
+    "url": "/covid19india/static/js/runtime-main.0a2b4a9a.js"
+  },
+  {
+    "revision": "b0f1593be4b4ab74e55a6ef3e4cae193",
+    "url": "/covid19india/static/media/covidlogo.b0f1593b.png"
+  }
+]);
